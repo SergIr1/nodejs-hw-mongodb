78fc931f8d8c031a7813da67372d9cfc6ced6006
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-05-20 20:14:54 +0100

    fix custom  middelware 404 and 500

--- src/middlewares/customMiddelware.js
+++ src/middlewares/customMiddelware.js
@@ -1,19 +1,15 @@
-import express from 'express';
-
-const app = express();
-
-export const middleware404 = app.use((req, res, next) => {
+export const middleware404 = (req, res, next) => {
   res.status(404).json({
     message: 'Not found',
   });
-});
+};
 
-export const middleware500 = app.use((err, req, res, next) => {
+export const middleware500 = (err, req, res, next) => {
   res.status(500).json({
     message: `Something went wrong`,
     error: err.message,
   });
-});
+};
 
 // console.log(typeof middleware404);
 
